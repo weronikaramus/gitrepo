22882fb8dd882fd8380ae80b86e42daabb90ae8d
nikolaCh6 <weronika.ramus@lo1.sandomierz.pl>
2018-10-30 13:31:23 +0100

    podstawy pythona

---
+++ python/prostokat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+a = input('Podaj bok a:')
+b = input('Podaj bok b:')
+
+obwod = 2 * (int(a) + int(b))
+print(obwod)
+
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
