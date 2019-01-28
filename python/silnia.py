45fa0621400f7ed6ef3431187f418959b62c6f8e
nikolaCh6 <weronika.ramus@lo1.sandomierz.pl>
2019-01-28 12:36:23 +0100

    potegi

---
+++ python/silnia.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  silnia.py
+#  
+# n! = 1 dla n = 0
+# n! = n * (n-1)! dla n >= 1
+# n! = 1 * ... * n
+# 4! = 1 * 2 * 3 * 4
+
+def silnia_it(n):
+    wynik = 1
+    for i in range(1, n + 1):
+        wynik = wynik * i
+    return wynik
+
+def main(args):
+    assert silnia_it(0, 0) == 0
+    assert silnia_it(10, 0) == 0
+    
+    
+    n = int(input("Podaj liczbę naturalną: "))
+    print("{}! = {}".format(n, silnia_it(n)))
+    
+    print(wynik)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+sys.exit(main(sys.argv))
