b936d780de79894fbac3d35f2fd58660d82622a1
weronikaramus <43135879+weronikaramus@users.noreply.github.com>
2019-03-04 19:19:13 +0100

    Update palindromy.py

--- python/palindromy.py
+++ python/palindromy.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
@@ -5,13 +6,13 @@
 import os
 
 
-def czy_palindrom(tekst):
+def czy_palindrom(tekst, teksty):
     ile = len(tekst)
     for i in range(ile // 2):
-        if tekst[i] != tekst[-i-1]:
+        if tekst[i] != tekst [-i-1]:
             return False
-            
     return True
+
 
 def czytaj_dane(plik):
     if not os.path.exists(plik):
@@ -19,22 +20,28 @@ def czytaj_dane(plik):
         return False
     teksty = []
     with open(plik, "r") as f:
-        for wiersz in f:
-            teksty.append(wiersz.strip())
-        return teksty
+        for linia in f:
+            teksty.append(linia.strip())
+    return teksty
+
 
 def main(args):
     # tekst = input('Podaj tekst: ')
     teksty = czytaj_dane('dane01.txt')
     print(teksty)
-    ile = 0
+    ileW = 0
+    if czy_palindrom(teksty):
+		ile += 1
+	else:
+		ile = ile
+	print(ile)
     
-    if czy_palindrom(tekst):
-        ile += 1
+    # ~if czy_palindrom(tekst):
+        # ~print('To palindrom!')
     # ~else:
-        # ~print('To nie palindrom!')
+        # ~print('To nie jest palindrom!')
     return 0
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
