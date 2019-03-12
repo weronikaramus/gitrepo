7e6eea665ca1477a2251ca141c93073ea1c7e65b
nikolaCh6 <weronika.ramus@lo1.sandomierz.pl>
2019-03-12 13:33:21 +0100

    tlumacz

--- python/tlumacz.py
+++ python/tlumacz.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 #
 #  tlumacz.py
+from random import randint
+import os
+import json
 
 def pokaz_menu():
     """Funkcja wyświetla działania dostępne dla użytkownia"""
@@ -40,10 +43,70 @@ def pobierzDane(dane):
             dane[slowo] = pobierzZnaczenia()
     else:
         dane[slowo] = pobierzZnaczenia()
-        
 
+def tlumacz(dane):
+    if not dane:
+        print('Brak słów.')
+        return
+    slowa = list(dane.keys())
+    op = 't'
+    while op == 't':
+        slowo = slowa[randint(0, len(slowa) - 1)]
+        print('Przetłumacz: ', slowo)
+        znaczenia = pobierzZnaczenia()
+        poprawne = [z for z in znaczenia if z in dane[slowo]]
+
+def tlimacz(dane):
+    if not dane:
+        print('Brak słów')
+        return
+    slowo = list(dane.keys())
+    op = 't'
+    while op == 't':
+        if len(slowa) > 1:
+            slowo = slowa[randint(0, len(slowa) - 1)]
+        else:
+            slow = slowa[0]
+        print('Przetłumacz: ', slowo)
+        znaczenia = pobierzZnaczenia()
+        poprawne = [z for z in znaczenia if z in dane(slowo)]
+        if  poprawne:
+            print('Poprawne:', ', '.join(poprawne))
+            slowa.remove(slowo)
+        else:
+            print('Brak poprawnych znaczeń')
+        if slowa:
+            op = input('Następne? (t/n)').lower()
+        else:
+            print('Przetłumaczyłeś wszystko!')
+            return
+
+def wczytaj_dane(plik, roz='.dat'):
+    dane = {}
+    if os.path.isfile(plik + roz):
+        with open(plik + roz, "r") as f:
+            dane = json.load(f)
+    else:
+        print('Plik {} nie istnieje.'.format(plik + roz))
+    return dane
+        
+def wybierzJezyk(konf_dane):
+    if konf_dane['jezyki']:
+        print('Wybierz język: ')
+        for i, j in enumerate(konf_dane['jezyki']):
+            print('{}. {}'.format(i + 1, j))
+        print('{}. nowy język'.format(i + 2))
+            
 def main(args):
-    dane = {'go': ['iść', 'jeździć'], 'see': ['widzieć', 'oglądać']}
+    # dane = {'go': ['iść', 'jeździć'], 'see': ['widzieć', 'oglądać']}
+    
+    konf_plik = 'baza'
+    konf_dane = wczytaj_dane(konf_plik)
+    if 'jezyki' not in konf_dane:
+        konf_dane['jezyki'] = []
+    jezyk = wybierzJezyk(konf_dane)
+    print(konf_dane)
+    return
     
     operacja = 0
     while operacja != 5:
@@ -53,7 +116,8 @@ def main(args):
             listaSlow(dane)
         elif operacja == 2:
             pobierzDane(dane)
-            pobierzDane(dane)
+        elif operacja == 3:
+            tlumacz(dane)
         elif operacja == 5:
             print('\nDo zobaczenia!')
         else:
