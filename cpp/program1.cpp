ef64aa4daf8e8f05f461fd89116e0c91628928ef
weronikaramus <weronika.ramus@lo1.sandomierz.pl>
2019-09-09 09:34:04 +0200

    program w C++

---
+++ cpp/program1.cpp
@@ -0,0 +1,18 @@
+/*
+ * program1.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int liczba1;
+    float liczba2;
+    cout << "Podaj liczbę całkowitą: ";
+    cin >> liczba1;
+    cout << "Podałeś: " << liczba1;
+	return 0;
+}
