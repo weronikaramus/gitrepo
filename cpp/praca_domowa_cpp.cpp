7cd9908ac65a4cb9f06fb2ec385d65ad6ee42ac6
weronikaramus <weronika.ramus@lo1.sandomierz.pl>
2019-10-09 23:24:22 +0200

    nie działa mi :(

---
+++ cpp/praca_domowa_cpp.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int staz = 1; int zarobek = 1000; int lata;
+
+void drukuj(){
+      cout << "Pracujesz " << staz << " lat i zarabiasz " << zarobek << "zł." << endl;  
+}
+
+void awans()
+{
+    for (staz=1; staz <= lata; staz += 1){
+
+    zarobek = zarobek + zarobek*(10/100);
+    cout << "Po " << staz << " lat pracy, Twoja pensja wyniesie " << zarobek << "zł." << endl;
+    
+}
+}
+
+int main()
+{
+    
+	cout << "Przewidywany okres pracy: ";
+    cin >> lata;
+    drukuj();
+    awans();
+	return 0;
+}
+
