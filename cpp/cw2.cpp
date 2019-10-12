e69eb8e96161c9e1380e0e0e3df1679b981d94ce
weronikaramus <weronika.ramus@lo1.sandomierz.pl>
2019-10-12 22:01:10 +0200

    prawie się udało

--- cpp/cw2.cpp
+++ cpp/cw2.cpp
@@ -2,12 +2,22 @@
 
 using namespace std;
 
+int oceny[20]; float suma=0, srednia;
 
 
 int main(int argc, char **argv)
 {
-    cout << "Wypisz swoje oceny: " cin >> o1 >>
+    for(int i=0; i<20; i++){
+        cout << endl << "Podaj ocenę: ";
+        cin >> oceny[i];
+        suma += oceny[i];
+        
+    }
+    // cout << suma << endl;
+    srednia = suma / 20;
+    cout << srednia << endl;
     
+        
     return 0;
 }
 
