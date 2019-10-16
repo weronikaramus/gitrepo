15a21e9e29ea03c11a085caa54b52f0bdba126f6
weronikaramus <weronika.ramus@lo1.sandomierz.pl>
2019-10-16 09:41:14 +0200

    uruchomić w domu

--- phpapp/inc/functions.php
+++ phpapp/inc/functions.php
@@ -1,4 +1,30 @@
 <?php
+
+$pages = array(
+	'witam' => 'Witamy',
+	'formularz' => 'Formularz',
+	'klasa' => 'Klasy'
+);
+
+function get_menu($id) {
+	global $pages;
+	foreach ($pages as $p => $t) {
+		echo '
+			<li class="nav-item">
+                <a class="nav-link" href="?id='.$p.'">'.$t.'</a>
+            </li>
+		';
+	}
+}
+
+function get_page_title($id) {
+	global $pages;
+	if (array_key_exists($id, $pages))
+		echo $pages[$id];
+	else
+		echo 'Aplikacja PHP';
+}
+
 function get_page_content($id) {
 	if (file_exists($id.'html'))
 		include($id.'html');
