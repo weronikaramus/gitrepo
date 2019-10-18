bd9ab6aba9b4725ed5173edd41d00ad04ace2bc3
weronikaramus <weronika.ramus@lo1.sandomierz.pl>
2019-10-18 09:41:07 +0200

    sql

--- phpapp/inc/db.php
+++ phpapp/inc/db.php
@@ -1,3 +1,11 @@
 <?php
+function init_baza() {
+	global $db, $ddbfile;
+	if (!file_exists(DBASE.$dbfile)) {
+		$kom[] = 'Brak pliku bazy. Tworzę nowy.';
+		$db = new PDO("sqlite:DBASE.$dbfile");
+	}
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXEPTION);
+}
 ;
 ?>
