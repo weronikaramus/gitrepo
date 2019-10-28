30112404d802d5024d8cb9cca4234dc4bd50b729
weronikaramus <weronika.ramus@lo1.sandomierz.pl>
2019-10-28 08:47:27 +0100

    tablice

--- cpp/tablica2w.cpp
+++ cpp/tablica2w.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-#define N   10
-#define M   10
+#define N   11
+#define M   11
 
 int main(int argc, char **argv)
 {
@@ -12,9 +12,9 @@ int main(int argc, char **argv)
     int i, j;
     
     srand(time(NULL)); //inicjacja generatora liczb pseudolosowych
-    for(i=1; i <= N; i++){
+    for(i=1; i < N; i++){
         // cout << "*** Tablica " << i << " ***" << endl;
-        for(j=1; j <= M; j++){
+        for(j=1; j < M; j++){
             // cout << i << "-" << j << endl;
             tab2w[i][j] = i*j;
             cout << setw(4) << tab2w[i][j] << " ";
